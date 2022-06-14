048e919d46df85ff9b20c5d582cb91ca19573ecd
Satya Ranjon <satyaranjonofficial@gmail.com>
2022-06-14 22:51:00 +0600

    change profile authore name

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
             <span>👋</span>
             <div className="" style={{ marginLeft: 20 }}>
               <p className="p-text"> Hello, I am</p>
-              <p className="head-text"> Khokon</p>
+              <p className="head-text"> Satya Ranjon</p>
               <h1></h1>
             </div>
           </div>
